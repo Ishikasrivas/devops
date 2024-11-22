dc3649fc4a0fee2a44ee04e866e5b1aa65707d30
Ishika Srivas <ishikasrivas35@gmail.com>
2024-11-22 16:52:45 +0530

    adding v2 verson

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-063d43db0594b521b"
     instance_type = "t2.micro"
     key_name = "dpp"
     security_groups = [ "demo-sg" ]
